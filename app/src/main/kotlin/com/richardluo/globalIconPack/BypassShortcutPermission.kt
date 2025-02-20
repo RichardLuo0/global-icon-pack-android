b1761077f3c77912cbeca54044330f5b1e8c5143
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2025-02-20 22:27:13 +0800

    refactor: make BypassShortcutPermission an object

--- app/src/main/kotlin/com/richardluo/globalIconPack/BypassShortcutPermission.kt
+++ app/src/main/kotlin/com/richardluo/globalIconPack/BypassShortcutPermission.kt
@@ -5,28 +5,26 @@ import com.richardluo.globalIconPack.utils.rGet
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
-class BypassShortcutPermission {
-  companion object {
-    fun onHookAndroid(lpp: LoadPackageParam) {
-      ReflectHelper.hookAllMethodsOrLog(
-        ReflectHelper.findClass("com.android.server.pm.LauncherAppsService\$LauncherAppsImpl", lpp)
-          ?: return,
-        "ensureShortcutPermission",
-        object : XC_MethodHook() {
-          override fun beforeHookedMethod(param: MethodHookParam) {
-            if (BuildConfig.APPLICATION_ID == param.args.rGet(-1)) param.result = null
-          }
-        },
-      )
-      ReflectHelper.hookAllMethodsOrLog(
-        ReflectHelper.findClass("com.android.server.pm.ShortcutService", lpp) ?: return,
-        "canSeeAnyPinnedShortcut",
-        object : XC_MethodHook() {
-          override fun beforeHookedMethod(param: MethodHookParam) {
-            if (BuildConfig.APPLICATION_ID == param.args[0]) param.result = true
-          }
-        },
-      )
-    }
+object BypassShortcutPermission {
+  fun onHookAndroid(lpp: LoadPackageParam) {
+    ReflectHelper.hookAllMethodsOrLog(
+      ReflectHelper.findClass("com.android.server.pm.LauncherAppsService\$LauncherAppsImpl", lpp)
+        ?: return,
+      "ensureShortcutPermission",
+      object : XC_MethodHook() {
+        override fun beforeHookedMethod(param: MethodHookParam) {
+          if (BuildConfig.APPLICATION_ID == param.args.rGet(-1)) param.result = null
+        }
+      },
+    )
+    ReflectHelper.hookAllMethodsOrLog(
+      ReflectHelper.findClass("com.android.server.pm.ShortcutService", lpp) ?: return,
+      "canSeeAnyPinnedShortcut",
+      object : XC_MethodHook() {
+        override fun beforeHookedMethod(param: MethodHookParam) {
+          if (BuildConfig.APPLICATION_ID == param.args[0]) param.result = true
+        }
+      },
+    )
   }
 }
