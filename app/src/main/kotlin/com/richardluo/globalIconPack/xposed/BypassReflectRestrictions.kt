f5758aa149060d9781169f52f699711aef43f794
Mickmick21 <michelangelochaume@gmail.com>
2025-07-20 11:37:05 +0200

    Update BypassReflectRestrictions.kt

--- app/src/main/kotlin/com/richardluo/globalIconPack/xposed/BypassReflectRestrictions.kt
+++ app/src/main/kotlin/com/richardluo/globalIconPack/xposed/BypassReflectRestrictions.kt
@@ -1,10 +1,13 @@
 package com.richardluo.globalIconPack.xposed
 
+import android.os.Build
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 import org.lsposed.hiddenapibypass.HiddenApiBypass
 
 object BypassReflectRestrictions {
   fun onHookSelf(@Suppress("UNUSED") lpp: LoadPackageParam) {
-    HiddenApiBypass.addHiddenApiExemptions("")
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+      HiddenApiBypass.addHiddenApiExemptions("")
+    }
   }
 }
