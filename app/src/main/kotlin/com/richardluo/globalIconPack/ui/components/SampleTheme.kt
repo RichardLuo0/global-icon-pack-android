9a1707c1ed4ab1896bde12a23decf2bd66c0ba3c
Mickmick21 <michelangelochaume@gmail.com>
2025-07-20 11:32:24 +0200

    Update SampleTheme.kt

--- app/src/main/kotlin/com/richardluo/globalIconPack/ui/components/SampleTheme.kt
+++ app/src/main/kotlin/com/richardluo/globalIconPack/ui/components/SampleTheme.kt
@@ -17,6 +17,7 @@
 package com.richardluo.globalIconPack.ui.components
 
 import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -38,15 +39,25 @@ fun SampleTheme(
   content: @Composable () -> Unit,
 ) {
   val context = LocalContext.current
-  val colorScheme =
-    when {
-      dynamicColor -> {
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+  val colorScheme = when {
+    dynamicColor -> {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        if (darkTheme) {
+          dynamicDarkColorScheme(context)
+        } else {
+          dynamicLightColorScheme(context)
+        }
+      } else {
+        if (darkTheme) {
+          darkColorScheme()
+        } else {
+          lightColorScheme()
+        }
       }
-
-      darkTheme -> darkColorScheme()
-      else -> lightColorScheme()
     }
+    darkTheme -> darkColorScheme()
+    else -> lightColorScheme()
+  }
 
   val view = LocalView.current
   if (!view.isInEditMode) {
